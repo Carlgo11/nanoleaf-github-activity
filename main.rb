ae939fd452c0da6840c5e0b454795d6fedd785c8
Carlgo11 <Carlgo11@users.noreply.github.com>
2021-03-28 04:00:34 +0200

    Fix rubocop warnings

--- main.rb
+++ main.rb
@@ -10,37 +10,34 @@ require 'open-uri'
 
 colors = %w[#ebedf0 #9be9a8 #40c463 #30a14e #216e39s]
 panels = %w[7678 24134 14755 43428 12412 55637 36730 28113 25017]
+data = []
 
-def fetchVar(name)
+def fetch_var(name)
   throw("Set environment variable '#{name}'.") if ENV[name].nil?
   ENV[name]
 end
 
-def fetchGitHubActivity(length)
-  html = Nokogiri::HTML.parse(URI.open("https://github.com/#{fetchVar('GITHUB_USER')}"))
+def fetch_github_activity(length)
+  html = Nokogiri::HTML.parse(URI.open("https://github.com/#{fetch_var('GITHUB_USER')}"))
   days = []
   boxes = html.css('rect.ContributionCalendar-day')
   boxes.each { |item| days.append(Integer(item['data-level'])) unless item['data-date'].nil? }
   days.last(length)
 end
 
-def sendPanelColors(data)
+# rubocop:disable Metrics/AbcSize
+def send_data(data)
   effect = JSON.parse(File.read('effect.json'))
   effect['write']['animData'] = data
-  url = URI("http://#{fetchVar('NANOLEAF_HOST')}/api/v1/#{fetchVar('NANOLEAF_TOKEN')}/effects")
-  http = Net::HTTP.new(url.host, url.port)
+  url = URI("http://#{fetch_var('NANOLEAF_HOST')}/api/v1/#{fetch_var('NANOLEAF_TOKEN')}/effects")
   request = Net::HTTP::Put.new(url)
   request.body = JSON.generate(effect)
-  response = http.start { |req| req.request(request) }
+  response = Net::HTTP.new(url.host, url.port).start { |http| http.request(request) }
   throw("Unable to change colors (#{response.code} #{response.body})") unless response.code.eql?('204')
 end
+# rubocop:enable Metrics/AbcSize
 
-days = fetchGitHubActivity(panels.length)
-puts "Activity levels: #{days.join(' ')}"
-data = []
-panels.each_with_index do |id, i|
-  color = colors[days[i]]
-  data << "#{id} 1 #{ColorConverter.rgb(color).join(' ')} 0 5"
-end
+days = fetch_github_activity(panels.length)
+panels.each_with_index { |id, i| data << "#{id} 1 #{ColorConverter.rgb(colors[days[i]]).join(' ')} 0 5" }
 
-sendPanelColors("#{panels.length} #{data.join(' ')}")
+send_data("#{panels.length} #{data.join(' ')}")
